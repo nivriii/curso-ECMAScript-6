1ab8fc2aa37c41fed32c640fd11e60f174f01355
nivriii <nivri.learning@gmial.com>
2021-04-09 21:14:31 -0500

    Se agrega comentario

--- src/es8/nuevas-caracteristicas-es8.js
+++ src/es8/nuevas-caracteristicas-es8.js
@@ -15,7 +15,7 @@ const values = Object.values(data);
 
 console.log(values);
 
-// 
+// padStart, padEnd
 const string = 'hello';
 console.log(string.padStart(7, 'hi'));
 console.log(string.padEnd(12, ' -----'));
