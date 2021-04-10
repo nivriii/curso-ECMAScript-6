1ffc4faed5ee9d809a354b77d16440a461eeaa1b
nivriii <nivri.learning@gmial.com>
2021-04-09 21:13:02 -0500

    Nuevas características ES8
    Métodos en objetos:
    - entries
    - values
    
    Métodos en strings:
    - padStart
    - padEnd

---
+++ src/es8/nuevas-caracteristicas-es8.js
@@ -0,0 +1,21 @@
+// entries
+const data = {
+    fronted: 'Ana',
+    backend: 'Irving',
+    design: 'Lucy'
+}
+
+const entries = Object.entries(data);
+
+console.table(entries);
+console.log(entries.length);
+
+// values
+const values = Object.values(data);
+
+console.log(values);
+
+// 
+const string = 'hello';
+console.log(string.padStart(7, 'hi'));
+console.log(string.padEnd(12, ' -----'));
